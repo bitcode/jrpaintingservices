28608cf2ea74a34c530a5350d96fd500daa844cd
bitcode <marcdrozrosario@gmail.com>
2023-06-05 11:39:23 +0000

    added card as background to hero on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 const HomePage: React.FC = () => {
   return (
     <>
+    <div className="card bg-base-200 mx-4">
     <div className="hero dark flex flex-col items-center justify-center py-40">
       <h1 className="hero-content text-4xl font-bold">Transform Your Living Space or Workplace Into a Masterpiece - Discover the Magic of Our Expert Painting Services!</h1> 
+      <p className="hero-content">Skyrocket Your Property Market Value - Experience the Power of Transformation with Our Superior Painting Services!</p>
+    </div>
+    </div>
+    <div>
+    normal content
     </div>
     </>
 
